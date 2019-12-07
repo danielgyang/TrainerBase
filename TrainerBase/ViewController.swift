60783649d74557a6f765623fc8682ae80cbbf77d
Daniel Yang <yangafc@bc.edu>
2019-12-06 23:34:02 -0500

    Initial Commit

---
+++ TrainerBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrainerBase
+//
+//  Created by Daniel Yang on 12/6/19.
+//  Copyright © 2019 Daniel Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
